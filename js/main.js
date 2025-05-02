e1cb647f1ce6ac0766268c8160143945f4a2e7eb
Michael Zeitler <zeimic@maincubes.com>
2025-05-02 14:11:34 +0200

    feat: logo animation via css and js (responsive)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 /** Initializes the app once the DOM is fully loaded. */
 function init() {
-    setTimeout(() => {
-        document.getElementById("splash-screen").classList.add("d-none");
-    }, 500);
+    handleSplashScreen();
 }
