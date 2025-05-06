5c25d6031f9fc04024767a5b3c6a52bb71d15106
Michael Zeitler <m.zeitler1987@gmail.com>
2025-05-06 14:56:12 +0200

    refactor: renderForm function for rendering both signup and login form depending on parameter

--- js/templates.js
+++ js/templates.js
@@ -3,7 +3,7 @@ function getSignupFormTemplate() {
             <section class="form-wrapper">
             <form class="pos-rel" action="submit">
                 <!-- BACK to Login Form -->
-                <button class="btn-small pos-abs back-signup">
+                <button onclick="renderForm(getLoginFormTemplate())" type="button" class="btn-small pos-abs back-signup">
                     <img class="icon-default" src="../assets/img/icon-back-default.svg">
                     <img class="icon-hover" src="../assets/img/icon-back-hover.svg">
                 </button>
@@ -54,5 +54,37 @@ function getSignupFormTemplate() {
                 </div>
             </form>
         </section>
+   `;
+}
+
+
+function getLoginFormTemplate() {
+    return /*html*/`
+            <section id="form-container" class="form-wrapper">
+                <form action="submit">
+                    <h1>Log in</h1>
+
+                    <!-- <input type="email" placeholder="Email" required> -->
+                    <div class="input-wrapper">
+                        <div class="input-area">
+                            <input type="email" placeholder="Email" required>
+                            <img src="../assets/img/icon-mail.svg" alt="">
+                        </div>
+                    </div>
+
+                    <!-- <input type="password" placeholder="Password" required> -->
+                    <div class="input-wrapper">
+                        <div class="input-area">
+                            <input type="password" placeholder="Password" required>
+                            <img src="../assets/img/icon-lock.svg" alt="">
+                        </div>
+                    </div>
+
+                    <div>
+                        <button class="btn-dark">Log in</button>
+                        <button class="btn-light"><b>Guest Log in</b></button>
+                    </div>
+                </form>
+            </section>
    `;
 }
