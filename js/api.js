f3643bce395ed54ec63150c0934ce50c7ee2af5f
Michael Zeitler <zeimic@maincubes.com>
2025-05-08 13:06:02 +0200

    feat: new function: getData for api requests

--- js/api.js
+++ js/api.js
@@ -1,5 +1,11 @@
 const FIREBASE_URL = "https://join-b179e-default-rtdb.europe-west1.firebasedatabase.app";
 
+async function getData(path="") {
+    let response = await fetch(FIREBASE_URL + path + ".json");
+    let responseAsJson = await response.json();
+    return responseAsJson;
+}
+
 /**
  * Loads all tasks from the Kanban board stored in the Firebase database.
  *
