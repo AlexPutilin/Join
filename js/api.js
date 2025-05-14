d3a4f0dae16dcd904415be7ab59995e97b9538bd
Aslihan Erdogan <asli.erdogan.gazel@gmail.com>
2025-05-14 09:48:24 +0200

    getData implemented in board

--- js/api.js
+++ js/api.js
@@ -1,6 +1,5 @@
 const FIREBASE_URL = "https://join-b179e-default-rtdb.europe-west1.firebasedatabase.app";
 
-
 /**
  * Fetches data from the Firebase Realtime Database at the specified path.
  * 
@@ -11,8 +10,8 @@ async function getData(path="") {
     let response = await fetch(FIREBASE_URL + path + ".json");
     let responseAsJson = await response.json();
     return responseAsJson;
+    
 }
-
 
 /**
  * Loads all tasks from the Kanban board stored in the Firebase database.
