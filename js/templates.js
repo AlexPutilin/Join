6d3a96e696e7dcfb82e89d964ed4c7bdc8def5bc
Janine Wächter <156570900+janinewchtr@users.noreply.github.com>
2025-06-30 14:35:43 +0200

    added the addTask page

--- js/templates.js
+++ js/templates.js
@@ -85,69 +85,85 @@ function getLoginFormTemplate() {
 }
 
 
-function getContactSelectionTemplate(param) {
-    return `
-        <div class="select-contact">
-            <div>
-                <div class="contact-icon">${param}</div>
-                <span>${param}</span>
+/**
+ * Returns the HTML template for displaying a selectable contact.
+ * @param {Object} param0 - Contact data.
+ * @param {string} param0.initials - Contact initials.
+ * @param {string} param0.name - Full name of the contact.
+ * @param {string|number} param0.id - Unique contact ID.
+ * @param {string} param0.color - Background color for the contact icon.
+ * @returns {string} HTML structure for a contact selection item.
+ */
+function getContactSelectionTemplate({ initials, name, id, color }) {
+  return `
+      <div class="select-contact">
+          <div>
+            <div class="icon-name-contact">
+              <div class="icon-contact" style="background-color: ${color}; color: white;">${initials}</div>
+              <span>${name}</span>
             </div>
-            <label class="checkbox">
-                <input type="checkbox" hidden>
-                <div class="icon-wrapper icon-checkbox-default">
-                    <img class="icon-default" src="../assets/img/icon-checkbutton-default.svg">
-                    <img class="icon-hover" src="../assets/img/icon-checkbutton-hover.svg">
-                </div>
-                <div class="icon-wrapper icon-checkbox-checked">
-                    <img class="icon-default" src="../assets/img/icon-checkbutton-checked-default.svg">
-                    <img class="icon-hover" src="../assets/img/icon-checkbutton-checked-hover.svg">
-                </div>
-            </label>
-        </div>`;
+          </div>
+          <label class="checkbox">
+              <input type="checkbox" hidden data-contact-id="${id}">
+              <div class="icon-wrapper icon-checkbox-default">
+                  <img class="icon-default" src="../assets/img/icon-checkbutton-default.svg">
+                  <img class="icon-hover" src="../assets/img/icon-checkbutton-hover.svg">
+              </div>
+              <div class="icon-wrapper icon-checkbox-checked">
+                  <img class="icon-default" src="../assets/img/icon-checkbutton-checked-default.svg">
+                  <img class="icon-hover" src="../assets/img/icon-checkbutton-checked-hover.svg">
+              </div>
+          </label>
+      </div>`;
 }
 
 
+/**
+ * Returns the HTML template for a single subtask item.
+ * @param {string} name - Subtask name or description.
+ * @returns {string} HTML structure for a subtask item.
+ */
 function getSubtaskTemplate(name) {
-    return `
-        <div class="subtask-item-container">
-            <div class="subtask-item">
-                <span class="subtask-name">• ${name}</span>
-                <div class="btn-collection-container">
-                    <button type="button" class="btn-small" onclick="openSubtaskEditMenu(this)">
-                        <div class="icon-wrapper">
-                            <img class="icon-default" src="../assets/img/icon-edit-default.svg">
-                            <img class="icon-hover" src="../assets/img/icon-edit-hover.svg">
-                        </div>
-                    </button>
-                    <div class="btn-seperator"></div>
-                    <button type="button" class="btn-small" onclick="deleteSubtaskItem(this)">
-                        <div class="icon-wrapper">
-                            <img class="icon-default" src="../assets/img/icon-delete-default.svg">
-                            <img class="icon-hover" src="../assets/img/icon-delete-hover.svg">
-                        </div>
-                    </button>
-                </div>
-            </div>
-            <div class="subtask-item-editmenu d-none">
-                <input type="text">
-                <div class="btn-collection-container">
-                    <button type="button" class="btn-small" onclick="closeAllSubtaskEdits()">
-                        <div class="icon-wrapper">
-                            <img class="icon-default" src="../assets/img/icon-cancel-task-default.svg">
-                            <img class="icon-hover" src="../assets/img/icon-cancel-task-hover.svg">
-                        </div>
-                    </button>
-                    <div class="btn-seperator"></div>
-                    <button type="button" class="btn-small" onclick="commitEditSubtask(this)">
-                        <div class="icon-wrapper">
-                            <img class="icon-default" src="../assets/img/icon-check-default.svg">
-                            <img class="icon-hover" src="../assets/img/icon-check-hover.svg">
-                        </div>
-                    </button>
-                </div>
-            </div>
-        </div>
-    `
+  return `
+      <div class="subtask-item-container">
+          <div class="subtask-item">
+              <span class="subtask-name">• ${name}</span>
+              <div class="btn-collection-container">
+                  <button type="button" class="btn-small" onclick="openSubtaskEditMenu(this)">
+                      <div class="icon-wrapper">
+                          <img class="icon-default" src="../assets/img/icon-edit-default.svg">
+                          <img class="icon-hover" src="../assets/img/icon-edit-hover.svg">
+                      </div>
+                  </button>
+                  <div class="btn-seperator"></div>
+                  <button type="button" class="btn-small" onclick="deleteSubtaskItem(this)">
+                      <div class="icon-wrapper">
+                          <img class="icon-default" src="../assets/img/icon-delete-default.svg">
+                          <img class="icon-hover" src="../assets/img/icon-delete-hover.svg">
+                      </div>
+                  </button>
+              </div>
+          </div>
+          <div class="subtask-item-editmenu d-none">
+              <input type="text">
+              <div class="btn-collection-container">
+                  <button type="button" class="btn-small" onclick="closeAllSubtaskEdits()">
+                      <div class="icon-wrapper">
+                          <img class="icon-default" src="../assets/img/icon-cancel-task-default.svg">
+                          <img class="icon-hover" src="../assets/img/icon-cancel-task-hover.svg">
+                      </div>
+                  </button>
+                  <div class="btn-seperator"></div>
+                  <button type="button" class="btn-small" onclick="commitEditSubtask(this)">
+                      <div class="icon-wrapper">
+                          <img class="icon-default" src="../assets/img/icon-check-default.svg">
+                          <img class="icon-hover" src="../assets/img/icon-check-hover.svg">
+                      </div>
+                  </button>
+              </div>
+          </div>
+      </div>
+  `;
 }
 
 
@@ -467,4 +483,273 @@ function getPriority(task) {
     } else {
         return "";
     }
+}
+
+/**
+ * Returns the HTML template for the "Add Task" form.
+ * @returns {string} HTML structure for the Add Task form section.
+ */
+function getAddTaskFormTemplate() {
+  return `
+    <section class="add-task-form">
+      <form id="add-task-form">
+        <div class="add-task-container">
+          <div class="mobile-h1">
+            <div><h1>Add Task</h1></div>
+          </div>
+          <div class="add-task-container-wrapper">
+            <div class="add-task-left">
+              <div id="title-wrapper-template">
+                ${getAddTaskTitleTemplate()}
+              </div>
+              <div id="description-wrapper-template">
+                ${getAddTaskDescriptionTemplate()}
+              </div>
+              <div id="due-date-wrapper-template">
+                ${getAddTaskDueDateTemplate()}
+              </div>
+            </div>
+
+            <div class="add-task-right">
+              <div id="priority-wrapper-template">
+                ${getPriorityTemplate()}
+              </div>
+              <div id="assigned-to-wrapper-template">
+                ${getAssignedToTemplate()}
+              </div>
+              <div id="category-wrapper-template">
+                ${getCategoryTemplate()}
+              </div>
+              <div id="subtask-wrapper-template">
+                ${getSubtaskInputTemplate()}
+              </div>
+            </div>
+          </div>
+        </div>
+        ${getaddTaskButtonsTemplate()}
+      </form>
+    </section>
+  `;
+}
+
+  
+/**
+ * Returns the HTML template for the task title input field.
+ * @returns {string} HTML structure for task title input.
+ */
+function getAddTaskTitleTemplate() {
+  return `
+  <div class="input-wrapper">
+    <div class="required-description">
+      <span>Title</span><span class="redstar">*</span>
+    </div>
+    <div class="input-area">
+      <input id="task-title" name="title" type="text" placeholder="Enter a title" required oninput="resetInputError()"/>
+    </div>
+    <span class="err-msg hidden">This field is required.</span>
+  </div>
+`;
+}
+
+/**
+ * Returns the HTML template for the task description textarea.
+ * @returns {string} HTML structure for task description input.
+ */
+function getAddTaskDescriptionTemplate() {
+  return `
+  <div class="input-wrapper">
+    <span>Description</span>
+    <div class="textarea">
+      <textarea name="description_full" placeholder="Enter a description"></textarea>
+    </div>
+  </div>
+`;
+}
+
+/**
+ * Returns the HTML template for the task due date input.
+ * @returns {string} HTML structure for due date field.
+ */
+function getAddTaskDueDateTemplate() {
+  return `              
+  <div class="input-wrapper">
+    <div class="required-description">
+      <span>Due date</span><span class="redstar">*</span>
+    </div>
+    <div class="input-area">
+      <input id="due-date" name="due_date" type="date" required/>
+    </div>
+    <span class="err-msg hidden">This field is required.</span>
+  </div>
+`;
+}
+
+/**
+ * Returns the HTML template for the subtask input section.
+ * @returns {string} HTML structure for subtask input field and buttons.
+ */
+function getSubtaskInputTemplate() {
+  return `
+    <div class="input-wrapper" id="subtask-input">
+      <span>Subtasks</span>
+      <div class="input-area">
+        <input id="subtask-input-field" type="text" placeholder="Add new subtask" data-placeholder="Add new subtask" data-placeholder-active="" oninput="toggleInputBtns(this)" onfocus="closeAllSubtaskEdits()">
+        <button type="button" class="btn-small" onclick="setInputFocus(this)">
+          <div class="icon-wrapper">
+            <img class="icon-default" src="../assets/img/icon-add-default.svg">
+            <img class="icon-hover" src="../assets/img/icon-add-hover.svg">
+          </div>
+        </button>
+        <div class="btn-collection-container d-none">
+          <button type="button" class="btn-small" onclick="resetSubtaskInput()">
+            <div class="icon-wrapper">
+              <img class="icon-default" src="../assets/img/icon-cancel-task-default.svg">
+              <img class="icon-hover" src="../assets/img/icon-cancel-task-hover.svg">
+            </div>
+          </button>
+          <div class="btn-seperator"></div>
+          <button type="button" class="btn-small" onclick="addSubtask()">
+            <div class="icon-wrapper">
+              <img class="icon-default" src="../assets/img/icon-check-default.svg">
+              <img class="icon-hover" src="../assets/img/icon-check-hover.svg">
+            </div>
+          </button>
+        </div>
+      </div>
+      <div class="list-subtasks"></div>
+      <div class="required-description form-error-mobile-visible">
+        <p class="redstar">*</p><p>This field is required</p>
+      </div>
+    </div>
+  `;
+}
+
+/**
+ * Returns the HTML template for the "Clear" and "Create Task" buttons.
+ * @returns {string} HTML structure for form action buttons.
+ */
+function getaddTaskButtonsTemplate() {
+  return `
+    <div class="clear-save-btn-wrapper">
+      <div class="clear-save-btn">
+        <div class="required-description form-error-desktop-visible">
+          <p class="redstar">*</p><p>This field is required</p>
+        </div>
+        <div class="clear-save-btn-pos">
+          <button type="button" class="btn-light" onclick="clearAddTaskForm()">
+            <span>Clear</span>
+            <div class="icon-wrapper">
+              <img class="icon-default" src="../assets/img/icon-cancel-default.svg">
+              <img class="icon-hover"   src="../assets/img/icon-cancel-hover.svg">
+              <img class="icon-active"  src="../assets/img/icon-cancel-click.svg">
+            </div>
+          </button>
+          <button type="button" class="btn-dark" id="create-task-btn" onclick="addTask()" disabled>
+            <span>Create Task</span>
+            <img src="../assets/img/icon-add-white.svg">
+          </button>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+/**
+ * Returns the HTML template for the category dropdown input.
+ * @returns {string} HTML structure for category selection.
+ */
+function getCategoryTemplate() {
+  return `
+    <div class="input-wrapper">
+      <div class="required-description">
+        <span>Category</span><span class="redstar">*</span>
+      </div>
+      <div class="input-area drop-down-input">
+        <input id="task-category" name="category" type="text" placeholder="Select task category" data-placeholder="Select task category" data-placeholder-active="Select task category" readonly required oninput="resetInputError(event)">
+        <button type="button" class="btn-small" onclick="toggleDropDown(this)">
+          <div class="icon-wrapper">
+            <img class="icon-default" src="../assets/img/icon-down-default.svg">
+            <img class="icon-hover" src="../assets/img/icon-down-hover.svg">
+          </div>
+          <div class="icon-wrapper d-none">
+            <img class="icon-default" src="../assets/img/icon-up-default.svg">
+            <img class="icon-hover" src="../assets/img/icon-up-hover.svg">
+          </div>
+        </button>
+      </div>
+      <div id="category-options-container" class="drop-down-menu d-none" data-open="false"></div>
+      <span class="err-msg hidden">This field is required.</span>
+    </div>
+  `;
+}
+
+/**
+ * Returns the HTML template for priority selection radio buttons.
+ * @returns {string} HTML structure for selecting task priority.
+ */
+function getPriorityTemplate() {
+  return `
+    <div class="input-wrapper">
+      <span>Priority</span>
+      <div class="priority-buttons-wrapper">
+        <label class="priority-option urgent">
+          <input name="priority" type="radio" value="urgent" hidden>
+          <span>Urgent</span>
+          <img src="../assets/img/icon-prio-urgent.svg" alt="">
+          <div class="bg-checked"></div>
+        </label>
+        <label class="priority-option medium">
+          <input name="priority" type="radio" value="medium" hidden checked>
+          <span>Medium</span>
+          <img src="../assets/img/icon-prio-medium.svg" alt="">
+          <div class="bg-checked"></div>
+        </label>
+        <label class="priority-option low">
+          <input name="priority" type="radio" value="low" hidden>
+          <span>Low</span>
+          <img src="../assets/img/icon-prio-low.svg" alt="">
+          <div class="bg-checked"></div>
+        </label>
+      </div>
+    </div>
+  `;
+}
+
+
+/**
+ * Returns the HTML template for the assigned contacts input and dropdown.
+ * @returns {string} HTML structure for assigning contacts.
+ */
+function getAssignedToTemplate() {
+  return `
+    <div class="input-wrapper">
+      <div class="required-description">
+        <span>Assigned to</span>
+      </div>
+      <div class="input-area drop-down-input">
+        <input type="text" name="assigned_to" id="assigned-input" placeholder="Select contacts to assign" data-placeholder="Select contacts to assign" data-placeholder-active="Search Contact"/>
+        <button type="button" class="btn-small" onclick="toggleDropDown(this)">
+          <div class="icon-wrapper">
+            <img class="icon-default" src="../assets/img/icon-down-default.svg" />
+            <img class="icon-hover" src="../assets/img/icon-down-hover.svg" />
+          </div>
+          <div class="icon-wrapper d-none">
+            <img class="icon-default" src="../assets/img/icon-up-default.svg" />
+            <img class="icon-hover" src="../assets/img/icon-up-hover.svg" />
+          </div>
+        </button>
+      </div>
+      <div id="contacts-dropdown" class="drop-down-menu d-none" data-open="false"></div>
+    </div>
+    <div class="assigned-chips" id="assigned-chips-container"></div>
+  `;
+}
+
+function getAddTaskNotificationTemplate() {
+  return `
+    <div class="successfully-added-notification btn-dark">
+      <span>Task added to board</span>
+      <img src="../assets/img/icon-board.svg" alt="Board icon">
+    </div>
+  `;
 }
